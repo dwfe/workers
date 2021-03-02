11023e95222a01b2ccb233fb5a01050abc358b38
z <alex@dwfe.ru>
2021-03-02 10:42:51 +0300

    .

--- packages/service-worker-skip-waiting/src/exchange/exchange.ts
+++ packages/service-worker-skip-waiting/src/exchange/exchange.ts
@@ -12,21 +12,24 @@ export class Exchange {
   constructor(private sw: SwEnv) {
   }
 
-  async send(type: MessageType, data) {
-    const clients = await self.clients.matchAll();
-    this.log(`send '${type}' to [${clients.length}] clients…`);
-    clients.forEach(client => client.postMessage({type, data}));
+  async send(type: MessageType, data, source?: ExtendableMessageEvent['source']) {
+    if (source) source.postMessage({type, data}, []); // отправить конкретному получателю
+    else {
+      const clients = await self.clients.matchAll();
+      this.log(`send '${type}' to [${clients.length}] clients…`);
+      clients.forEach(client => client.postMessage({type, data}));
+    }
   }
 
   async process(event: IMessageEvent) {
-    const {data} = event;
+    const {data, source} = event;
     this.log(`process '${data.type}'`);
 
     switch (data.type) {
       case 'GET_INFO':
         this.send('INFO', {
           caches: await this.sw.cache?.info()
-        });
+        }, source);
         break;
       default:
         throw new Error(`sw unknown message type '${data.type}' of Exchange.process(…)`);
